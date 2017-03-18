a714ae0f2d3e9bfa20f82a8346fcf733486b2e41
kosinix <kosinix@users.noreply.github.com>
2017-03-18 17:55:29 +0800

    Remove glob, add gulp-zip and new tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,9 @@
 var gulp = require('gulp');
-var glob = require('glob');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var sourcemaps = require('gulp-sourcemaps');
+var zip = require('gulp-zip');
 
 var jsOutPath = 'build/cyclone-slider-2/js';
 var jsIn = [
@@ -26,4 +26,33 @@ gulp.task('js', function() {
         .pipe(gulp.dest(jsOutPath));
 
 });
-gulp.task('default', ['js']);
+
+var releaseIn = [
+    'css/**',
+    'images/**',
+    'js/**',
+    'languages/**',
+    'libs/**',
+    'src/**',
+    'templates/**',
+    'views/**',
+    'cyclone-slider.php',
+    'README.txt'
+];
+var releaseOutPath = 'release/cyclone-slider-2';
+gulp.task('release', function() {
+
+    return gulp.src(releaseIn, {base:'.'}) // Base preserves the dir structure
+        .pipe(gulp.dest(releaseOutPath));
+
+});
+
+gulp.task('zip', function() {
+
+    return gulp.src('release/cyclone-slider-2/**', {base:'release'}) // Base preserves the dir structure
+        .pipe(zip('cyclone-slider-2.zip'))
+        .pipe(gulp.dest('release'));
+
+});
+
+gulp.task('default', ['js']); // Runs on "gulp" command only
